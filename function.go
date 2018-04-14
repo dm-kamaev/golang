e47824fc3a25f3d978197cbb42a37d1d01801c15
Dmitrii K <dm-kamaev@rambler.ru>
2018-04-14 10:25:54 +0300

    work with function

---
+++ function.go
@@ -0,0 +1,181 @@
+//usr/local/bin/go run $0 $@ ; exit
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	// "reflect"
+)
+
+func main() {
+	// res := average([5]float64{1, 2, 3, 4, 5})
+	// fmt.Println(res)
+
+	// fmt.Println(retun_many())
+
+	// fmt.Println(many_ints(1, 2, 3, 4))
+
+	// fmt.Println(any_argv(5, 2, 3, 4))
+
+	// counter := Counter()
+	// fmt.Println(counter(10))
+	// fmt.Println(counter(11))
+
+	// fmt.Println(factorial(4))
+	// test_read_file()
+	// test_defer()
+	// test_recover()
+	// fmt.Println(task1([]uint{1, 2, 3, 4}))
+
+	// ===========================
+	// fmt.Println(task2(1))
+	// fmt.Println(task2(2))
+	// fmt.Println(task2(4))
+	// fmt.Println(task2(10))
+	// fmt.Println(task2(13))
+	// ===========================
+
+	// fmt.Println(search_min(100, 2, 3, 4, 5, -1))
+
+	// ===========================
+	// oddGenerator := makeOddGenerator()
+	// fmt.Println(oddGenerator())
+	// fmt.Println(oddGenerator())
+	// fmt.Println(oddGenerator())
+	// fmt.Println(oddGenerator())
+	// ===========================
+}
+
+func average(ar [5]float64) float64 {
+	total := 0.0
+	for _, val := range ar {
+		total += val
+	}
+	return total / float64(len(ar))
+}
+
+func retun_many() ([]int, []int) {
+	x := []int{1, 2}
+	y := []int{2, 3}
+	return x, y
+}
+
+func many_ints(arg ...int) int {
+	sum := 0
+	for _, val := range arg {
+		sum += val
+	}
+	return sum
+}
+
+func any_argv(arg ...interface{}) int {
+	sum := 0
+	for _, val := range arg {
+		sum += val.(int) // convert interface to int
+	}
+	return sum
+}
+
+func Counter() func(int) int {
+	i := 0
+	return func(add int) int {
+		i = (i + 1) + add
+		return i
+	}
+}
+
+func factorial(num uint) uint {
+	if num == 0 {
+		return 1
+	}
+	return num * factorial(num-1)
+}
+
+func test_read_file() {
+	data, err := ioutil.ReadFile("./test.txt")
+	fmt.Println(string(data), err)
+}
+
+func test_defer() {
+
+	first := func() {
+		fmt.Println("first")
+	}
+
+	second := func() {
+		fmt.Println("second")
+	}
+
+	// always call before end
+	defer second()
+	// panic("ERROR")
+	first()
+}
+
+func test_recover() {
+	defer func() {
+		str := recover()
+		fmt.Println("ERROR= " + str.(string))
+	}()
+
+	panic("!!!OOpps ERROR!!!")
+}
+
+//Функция sum принимает срез чисел и складывает их вместе. Как бы выглядела сигнатура этой функции?
+func task1(list []uint) uint {
+	var total uint = 0
+	for _, val := range list {
+		total += val
+	}
+	return total
+}
+
+// Напишите функцию, которая принимает число,
+// делит его пополам и возвращает true в случае, если получившееся число чётное, и false в случае нечетного результата.
+// Например, half(1) должна вернуть (0, false), в то время как half(2) вернет (1, true).
+func task2(num float64) (int, bool) {
+	half := int(num / 2)
+
+	if half == 0 {
+		return 0, false
+	}
+
+	is_even := (half % 2) == 0
+	if is_even {
+		return 1, true
+	} else {
+		return 0, false
+	}
+}
+
+func search_min(argv ...int) int {
+	var min int
+	for _, val := range argv {
+		if val < min {
+			min = val
+		}
+	}
+	return min
+}
+
+func makeOddGenerator() func() int {
+	var i int
+	return func() int {
+		if i == 0 {
+			i++
+			return 0
+		} else {
+			if i%2 != 0 {
+				old := i
+				i++
+				return old
+			} else {
+				i++
+				old := i
+				i++
+				return old
+			}
+		}
+	}
+}
--- main.go
+++ main.go
@@ -4,148 +4,154 @@ package main
 
 import "fmt"
 
-
-
-
-
-
-// func main() {
-// 	// init hash
-// 	elements := map[string]map[string]string{
-// 		"H": map[string]string{
-// 			"name":  "Hydrogen",
-// 			"state": "gas",
-// 		},
-// 		"He": map[string]string{
-// 			"name":  "Helium",
-// 			"state": "gas",
-// 		},
-// 		"Li": map[string]string{
-// 			"name":  "Lithium",
-// 			"state": "solid",
-// 		},
-// 		"Be": map[string]string{
-// 			"name":  "Beryllium",
-// 			"state": "solid",
-// 		},
-// 		"B": map[string]string{
-// 			"name":  "Boron",
-// 			"state": "solid",
-// 		},
-// 		"C": map[string]string{
-// 			"name":  "Carbon",
-// 			"state": "solid",
-// 		},
-// 		"N": map[string]string{
-// 			"name":  "Nitrogen",
-// 			"state": "gas",
-// 		},
-// 		"O": map[string]string{
-// 			"name":  "Oxygen",
-// 			"state": "gas",
-// 		},
-// 		"F": map[string]string{
-// 			"name":  "Fluorine",
-// 			"state": "gas",
-// 		},
-// 		"Ne": map[string]string{
-// 			"name":  "Neon",
-// 			"state": "gas",
-// 		},
-// 	}
-
-// 	if el, ok := elements["Li"]; ok {
-// 		fmt.Println(el["name"], el["state"])
-// 	}
+func main() {
+	simple_for()
+	calc_total()
+	for_range()
+	compact_array()
+	example_slice()
+	exmaple_append()
+	exmaple_copy()
+	exmaple_hash()
+	work_with_hash()
 }
 
-// func main() {
-// 	hash := make(map[string]string) // OR var hash = make(map[string]string)
-// 	hash["test"] = "hello world"
-// 	fmt.Println(hash)
-// 	key := "something"
-// 	if val, ok := hash[key]; !ok {
-// 		fmt.Println("Not exist val '" + val + "' ")
-// 	}
-// }
+func work_with_hash() {
+	// init hash
+	elements := map[string]map[string]string{
+		"H": map[string]string{
+			"name":  "Hydrogen",
+			"state": "gas",
+		},
+		"He": map[string]string{
+			"name":  "Helium",
+			"state": "gas",
+		},
+		"Li": map[string]string{
+			"name":  "Lithium",
+			"state": "solid",
+		},
+		"Be": map[string]string{
+			"name":  "Beryllium",
+			"state": "solid",
+		},
+		"B": map[string]string{
+			"name":  "Boron",
+			"state": "solid",
+		},
+		"C": map[string]string{
+			"name":  "Carbon",
+			"state": "solid",
+		},
+		"N": map[string]string{
+			"name":  "Nitrogen",
+			"state": "gas",
+		},
+		"O": map[string]string{
+			"name":  "Oxygen",
+			"state": "gas",
+		},
+		"F": map[string]string{
+			"name":  "Fluorine",
+			"state": "gas",
+		},
+		"Ne": map[string]string{
+			"name":  "Neon",
+			"state": "gas",
+		},
+	}
 
-// func main() {
-// 	slice_1 := []int{1, 2, 3}
-// 	slice_2 := make([]int, 2)
-// 	// create new array, but not modification exist array
-// 	copy(slice_2, slice_1)
-// 	slice_1[0] = 100
-// 	fmt.Println(slice_1)
-// 	fmt.Println(slice_2)
-// }
+	if el, ok := elements["Li"]; ok {
+		fmt.Println(el["name"], el["state"])
+	}
+}
 
-// func main() {
-// 	slice_1 := []int{1, 2, 3}
-// 	// create new array, but not modification exist array
-// 	slice_2 := append(slice_1, 4, 5, 6)
-// 	slice_1[0] = 100
-// 	fmt.Println(slice_1)
-// 	fmt.Println(slice_2)
-// }
+func exmaple_hash() {
+	hash := make(map[string]string) // OR var hash = make(map[string]string)
+	hash["test"] = "hello world"
+	fmt.Println(hash)
+	key := "something"
+	if val, ok := hash[key]; !ok {
+		fmt.Println("Not exist val '" + val + "' ")
+	}
+}
 
-// func main() {
-// 	// slice uses memory, which use original array
-// 	ar := [5]int{1, 2, 3, 4, 5}
-// 	slice := ar[0:3]
-// 	fmt.Println(ar)
-// 	fmt.Println(slice)
-// 	ar[0] = 200
-// 	fmt.Println()
-// 	fmt.Println(ar)
-// 	fmt.Println(slice)
-// 	slice[10] = 2
-// }
+func exmaple_copy() {
+	slice_1 := []int{1, 2, 3}
+	slice_2 := make([]int, 2)
+	// create new array, but not modification exist array
+	copy(slice_2, slice_1)
+	slice_1[0] = 100
+	fmt.Println(slice_1)
+	fmt.Println(slice_2)
+}
 
-// func main() {
-// 	ar := [5]float64{1, 2, 3, 4, 5}
-// 	var total float64 = 0
-// 	for _, val := range ar {
-// 		total += val
-// 	}
-// 	fmt.Println(total / float64(len(ar)))
-// }
+func exmaple_append() {
+	slice_1 := []int{1, 2, 3}
+	// create new array, but not modification exist array
+	slice_2 := append(slice_1, 4, 5, 6)
+	slice_1[0] = 100
+	fmt.Println(slice_1)
+	fmt.Println(slice_2)
+}
 
-// func main() {
-// 	var ar [5]float64
-// 	ar[0] = 1
-// 	ar[1] = 2
-// 	ar[2] = 3
-// 	ar[3] = 4
-// 	ar[4] = 5
-// 	var total float64 = 0
-// 	for _, val := range ar {
-// 		total += val
-// 	}
-// 	fmt.Println(total / float64(len(ar)))
-// }
+func example_slice() {
+	// slice uses memory, which use original array
+	ar := [5]int{1, 2, 3, 4, 5}
+	slice := ar[0:3]
+	fmt.Println(ar)
+	fmt.Println(slice)
+	ar[0] = 200
+	fmt.Println()
+	fmt.Println(ar)
+	fmt.Println(slice)
+}
 
-// func main() {
-// 	var ar [5]float64
-// 	ar[0] = 1
-// 	ar[1] = 2
-// 	ar[2] = 3
-// 	ar[3] = 4
-// 	ar[4] = 5
-// 	var total float64 = 0
-// 	for i := 0; i < len(ar); i++ {
-// 		total += ar[i]
-// 	}
-// 	fmt.Println(total / float64(len(ar)))
-// }
+func compact_array() {
+	ar := [5]float64{1, 2, 3, 4, 5}
+	var total float64 = 0
+	for _, val := range ar {
+		total += val
+	}
+	fmt.Println(total / float64(len(ar)))
+}
 
-// func main() {
-// 	var ar [5]float64
-// 	ar[0] = 1
-// 	ar[1] = 2
-// 	ar[2] = 3
-// 	ar[3] = 4
-// 	ar[4] = 5
-// 	for i := 0; i < len(ar); i++ {
-// 		fmt.Println(ar[i])
-// 	}
-// }
+func for_range() {
+	var ar [5]float64
+	ar[0] = 1
+	ar[1] = 2
+	ar[2] = 3
+	ar[3] = 4
+	ar[4] = 5
+	var total float64 = 0
+	for _, val := range ar {
+		total += val
+	}
+	fmt.Println(total / float64(len(ar)))
+}
+
+func calc_total() {
+	var ar [5]float64
+	ar[0] = 1
+	ar[1] = 2
+	ar[2] = 3
+	ar[3] = 4
+	ar[4] = 5
+	var total float64 = 0
+	for i := 0; i < len(ar); i++ {
+		total += ar[i]
+	}
+	fmt.Println(total / float64(len(ar)))
+}
+
+func simple_for() {
+	var ar [5]float64
+	ar[0] = 1
+	ar[1] = 2
+	ar[2] = 3
+	ar[3] = 4
+	ar[4] = 5
+	for i := 0; i < len(ar); i++ {
+		fmt.Println(ar[i])
+	}
+}
